70993ceb044880145fab0499d8cdb675b5155303
Roman Kolivashko <rkolivashko@gmail.com>
2021-09-04 20:23:06 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+//Write a function to find the average valuein an array of numbers
+//avg([0,50]) //25
+//avg([75,76,80,95,100]) //85.2
+
+function avg(arr){
+  let total = 0;
+  //loop over each num
+  for (let num of arr) {
+    //add them together
+    total += num;
+  }
+  //divide by number of nums
+  let res = total / arr.length;
+  return res;
+}
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $(".content").click(function() {
+//     $(".card-showing").toggle();
+//     $(".card-hidden").toggle();
+//   });
+// });
+
+// $(document).ready(function() {
+//   attachContactListeners();
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//     const inputtedFirstName = $("input#new-first-name").val();
+//     const inputtedLastName = $("input#new-last-name").val();
+//     const inputtedPhoneNumber = $("input#new-phone-number").val();
+
+//     // The next three lines are new:
+//     $("input#new-first-name").val("");
+//     $("input#new-last-name").val("");
+//     $("input#new-phone-number").val("");
+
+//     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+//     addressBook.addContact(newContact);
+//     displayContactDetails(addressBook);
+//   });
+// });
